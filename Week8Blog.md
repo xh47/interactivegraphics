f1abe5d2a69c342b8338e1cc9a51ee1c172d85fd
Ian Hua <xh47@duke.edu>
2018-02-27 21:59:20 -0500

    week 8

---
+++ Week8Blog.md
@@ -0,0 +1,23 @@
+# Week 8 Blog Post
+## Ian Hua | Interactive Graphics
+
+### Art Piece 1
+Pitch Interactive
+
+Out of Sight, Out of Mind.
+
+http://drones.pitchinteractive.com/
+
+This is one of the coolest and most interesting uses of data I have seen - the drone strikes and their corresponding amount of deaths are displayed through drone strike visualizations along a year axis. You can then hover over the data and receive further information on a certain drone strike and the amount of people affected. From the visualization you can clearly see the negative impact of drone strikes. I think the color scheme and visualization in general is very well done, attractive, and gets the point across very well.
+
+
+
+
+### Art Piece 2
+Martin Wattenberg and Fernanda Viégas
+
+Wind Map (2012)
+
+http://hint.fm/wind/
+
+This one is a really cool piece by Martin Wattenberg. Honestly I haven't seen someone try to visualize wind in current time and the wind map that they have created is not only artistic but gives you a general sense of the weather in the US! In addition the wind map is very accurate, especially during hurricanes! Screenshots of the wind map produce very interesting designs due to the comet-like trails implemented for the wind display. I really enjoyed learning more about this piece. 
--- sketch.js
+++ sketch.js
@@ -1,31 +1,82 @@
 var speedmax = 3;
-var n = 50;
+var n = 70;
 var img;
 
 class mol {
-    constructor(x, y, sx, sy, col, sizee){
-        this.x = random(width);
-        this.y = random(height);
-        this.sx = random(-speedmax,speedmax);
-        this.sy = random(-speedmax,speedmax);
-        this.col = color(random(255), random(255), random(255),random(20,100));
-        this.sizee = random(10,50)
+    constructor(x1, y1, sx1, sy1, col1, sizee1){
+        this.x = x1;
+        this.y = y1;
+        this.sx = sx1;
+        this.sy = sy1;
+        this.col = col1;
+        this.sizee = sizee1;
+    }
+
+    display(){
+        this.x += this.sx;
+        this.y += this.sy;
+
+        if (mouseX < width & mouseX > 0 & mouseY < height & mouseY > 0){
+            this.sx -= 0.25*(this.x - mouseX)/width
+            this.sy -= 0.25*(this.y - mouseY)/height
+        }
+        
+        if (this.x > width+this.sizee){
+            this.x = 0-this.sizee;
+            this.sx = random(-speedmax,speedmax);
+            this.sy = random(-speedmax,speedmax);
+        }
+        if (this.x < 0-this.sizee){
+            this.x = width+this.sizee;
+            this.sx = random(-speedmax,speedmax);
+            this.sy = random(-speedmax,speedmax);
+        }
+        if (this.y > height+this.sizee){
+            this.y = 0-this.sizee;
+            this.sx = random(-speedmax,speedmax);
+            this.sy = random(-speedmax,speedmax);
+        }
+        if (this.y < 0-this.sizee){
+            this.y = height+this.sizee;
+            this.sx = random(-speedmax,speedmax);
+            this.sy = random(-speedmax,speedmax);
+        }        
+        fill(this.col);
+        noStroke();
+        ellipse(this.x, this.y, this.sizee, this.sizee);
     }
 }
 
 var mols = new Array(n);
 
+var table;
+function preload(){
+    table = loadTable("data.csv", "header")
+}
+
+var years = [];
+var co2 = [];
+
 function setup(){
     createCanvas(windowWidth-20,windowHeight-100)
     //img = loadImage("images/background.jpeg");
     for (var i = 0; i < n; i++){
-        var m = new mol();
+        var m = new mol(random(width),random(height),random(-speedmax,speedmax),random(-speedmax,speedmax),color(random(200,255), random(200,255), random(200,255),random(20,100)),random(10,50));
         mols[i] = m;
     }
     frameRate(120);
     cursor("images/blackhole.png", 16,16);
+
+    loadData();
 }
 
+function loadData(){
+    years = table.getColumn(1);
+    co2 = table.getColumn(0);
+}
+
+var clicks = -1;
+var middletext = '';
 
 function draw(){
     //createCanvas(windowWidth-20,windowHeight-100);
@@ -33,37 +84,27 @@ function draw(){
     //image(img, 0, 0, img.width, img.height);
 
     for (var i = 0; i < n; i++){
-        mols[i].x += mols[i].sx;
-        mols[i].y += mols[i].sy;
-
-        if (mouseX < width & mouseX > 0 & mouseY < height & mouseY > 0){
-            mols[i].sx -= 0.25*(mols[i].x - mouseX)/width
-            mols[i].sy -= 0.25*(mols[i].y - mouseY)/height
-        }
-        
-        if (mols[i].x > width+mols[i].sizee){
-            mols[i].x = 0-mols[i].sizee;
-            mols[i].sx = random(-speedmax,speedmax);
-            mols[i].sy = random(-speedmax,speedmax);
-        }
-        if (mols[i].x < 0-mols[i].sizee){
-            mols[i].x = width+mols[i].sizee;
-            mols[i].sx = random(-speedmax,speedmax);
-            mols[i].sy = random(-speedmax,speedmax);
-        }
-        if (mols[i].y > height+mols[i].sizee){
-            mols[i].y = 0-mols[i].sizee;
-            mols[i].sx = random(-speedmax,speedmax);
-            mols[i].sy = random(-speedmax,speedmax);
-        }
-        if (mols[i].y < 0-mols[i].sizee){
-            mols[i].y = height+mols[i].sizee;
-            mols[i].sx = random(-speedmax,speedmax);
-            mols[i].sy = random(-speedmax,speedmax);
-        }
-        ellipse(mols[i].x, mols[i].y, mols[i].sizee, mols[i].sizee);
-        fill(mols[i].col);
-        noStroke();
+        mols[i].display();
     }
 
+    textSize(32);
+    textAlign(CENTER);
+    fill(0,0,0)
+    text(middletext, width/2, height/2);
+
+}
+
+
+function mousePressed() {
+    clicks = clicks + 1;
+    if (clicks < years.length){
+        middletext = years[clicks];
+        for (var i = 0; i < n; i++){
+            var ratio = co2[clicks]/co2[co2.length-1];
+            mols[i].sizee = random(0.75,1.25)*ratio*500;
+            mols[i].col = color((1-ratio)*random(200,255), (1-ratio)*random(200,255), (1-ratio)*random(200,255), random(20,50));
+        }
+    } else {
+        middletext = "This is a real problem."
+    }
 }
